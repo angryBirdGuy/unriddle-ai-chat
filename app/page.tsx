bd16442f7cf2c1bc13d409364294e5b98a96999e
Man Nguyen <mantnguy@gmail.com>
2024-06-04 17:40:55 -0500

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,18 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col p-6 border max-w-xl w-full gap-6 rounded-md bg-card">
-        <div className="flex justify-between">
-          <h1 className="text-4xl font-semibold">AI Chat Component</h1>
-          <ThemeToggle />
+      <div className="absolute top-4 right-4">
+        <ThemeToggle />
+      </div>
+      <div className="flex flex-col p-6 border max-w-2xl w-full gap-6 rounded-md bg-card shadow-2xl">
+        <div className="flex justify-center">
+          <h1 className="text-[2rem] font-semibold sm:text-[3rem] mb-5 sm:mb-6 leading-[0.9] !tracking-[-0.2rem]">
+            AI Chat Component
+          </h1>
         </div>
-        <Editor initialValue={value} onChange={setValue} />
+        <div className="text-base">
+          <Editor initialValue={value} onChange={setValue} />
+        </div>
       </div>
     </main>
   );
--- components/editor/advanced-editor.tsx
+++ components/editor/advanced-editor.tsx
@@ -59,7 +59,7 @@ const Editor = ({ initialValue, onChange }: EditorProp) => {
   return (
     <EditorRoot>
       <EditorContent
-        className="border p-4 rounded-xl"
+        className="border p-8 rounded-xl text-base font-medium"
         {...(initialValue && { initialContent: initialValue })}
         extensions={extensions}
         editorProps={{
